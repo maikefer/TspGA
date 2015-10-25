24a80b5dc69051a10f8695c54e26be5d570c6ebd
Maike Rees <maikefer_@gmx.de>
2015-10-25 13:48:16 -0400

    blabla

--- src/geneticAlgorithms/Individual.java
+++ src/geneticAlgorithms/Individual.java
@@ -1,25 +1,46 @@
 package geneticAlgorithms;
 
+import java.math.*;
+import java.util.Random;
+
 public class Individual {
 	
-	private char[] representation;
+	private char[] cities;
 	private int length;
 	
 	public Individual(int citySize){
 		this.length = citySize;
+		this.cities = new char[10];
 		this.initializeRandomly();
 	}
 	
 	public int getFitness(){
+		// always every city only one time inside
+		// fitness = distance traveled the smaller the better
 		return 0;
 	}
 	
 	public void initializeRandomly(){
+		int city = 0;
+		Random generator = new Random();
 		
+		for (int i = 0; i < length; i++) {
+			city = Math.abs(generator.nextInt()  % (length));
+			
+			// '\u0000' is the default value for chars --> char hasn't been initialized
+			if (cities[city] == '\u0000') {
+				cities[city] = Integer.toString(i).charAt(0);
+			}
+		}
 	}
 	
 	public void mutate(){
 		
+	}
+	
+	@Override
+	public String toString(){
+		return new String(cities);
 	}
 
 }
--- src/geneticAlgorithms/RunMe.java
+++ src/geneticAlgorithms/RunMe.java
@@ -4,6 +4,15 @@ public class RunMe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// a city will be represented by a number
+		
+		Individual indiv;
+		
+		for (int i = 0; i < 100; i++){
+			indiv = new Individual(9);
+			System.out.println(i + ": " + indiv.toString());
+		}
 
 	}
 
